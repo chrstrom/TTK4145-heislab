777625aee3e5f4ed0abcb0ffd2871eacea6e92de
Christopher Strøm <chstr@stud.ntnu.no>
2021-03-03 20:25:42 +0100

    cleanup FSM and fix typos
    Signed-off-by: Christopher Strøm <chstr@stud.ntnu.no>

--- fsm/fsm.go
+++ fsm/fsm.go
@@ -1,8 +1,13 @@
 package fsm
 
+import (
+	"fmt"
+	io "../elevio"
+)
+
 // Orders are received from the elevator server, so it makes
 // sense for the switch/cases to be replaced with a for/select
-// 
+//
 // The functions would then also take in a channel, defined in main.go
 // Internal state will change as normal by setting something equals
 // to something else, but the outputs should also go on channels
@@ -10,71 +15,74 @@ package fsm
 // This also means that the "elevator output device" will not exist
 // Outputs instead reach the elevator through the elevator server
 
-// In this implementation the output channels are replaced by hardware actions
-// For the actual 
+// Making this variable available to the entire fsm package makes sense, since
+// it makes us not have to pass the elevator around as an argument everywhere.
+// The for-select block in RunElevatorFSM() will at all times only execute
+// one case, which means that we will not run into concurrency problems here either
+var elevator = makeUninitializedElevator()
 
 
-
-func makeUninitializedElevator() *Elevator {
+func makeUninitializedElevator() Elevator {
 	elevator := new(Elevator)
 	elevator.floor = -1
-	elevator.direction = Stop
+	elevator.direction = io.MD_Stop
 	// 2D array of requests is 0 by default
 	elevator.state = Idle
 
-	return elevator
+	return *elevator
 }
 
+func onRequestButtonPress(button_msg io.ButtonEvent) {
 
-
-
-func onRequestButtonPress(button_msg ButtonEvent) {
-
-	button_floor = button_msg.Floor
-	button_type = button_msg.Button
+	var button_floor = button_msg.Floor
+	var button_type = button_msg.Button
 
 	switch elevator.state {
 
 	case DoorOpen:
-		if(elevator.floor = button_floor) {
-			timer_start(DOOR_OPEN_DURATION)
-		}
-		else {
-			ele
-			
-			vator.requests[button_floor][button_type] = 1
+		if elevator.floor == button_floor {
+			fmt.Printf("Opening door!")
+			//timer_start(DOOR_OPEN_DURATION)
+		} else {
+			elevator.requests[button_floor][button_type] = 1
 		}
 
 	}
+}
+
+func onFloorArrival(floor int) {
+
 }
 
 
 // This function is the function from the fsm package that will run
 // as a goroutine. Because of this, it should take inputs based on
-// channels, and the for-select will take care of the 
-//
-//
-//
-func RunElevatorFSM(atFloor       FloorArrival <-chan,
-		    buttonPress   ButtonPress  <-chan)
-{
+// channels, and the for-select will take care of the
+func RunElevatorFSM(event_orderButton 	<-chan io.ButtonEvent,
+					event_floorArrival 	<-chan int,
+					event_obstruction 	<-chan bool,
+					event_stopButton 	<-chan bool) {
 
-	// Initialize
-	elevator = makeUninitializedElevator()
-
-	// Loops indefinitely. RunElevatorFSM is a goroutine.
+	// Loops indefinitely. RunElevatorFSM *should be* a goroutine.
 	for {
 
 		select {
 
-		case newFloor := <-atFloor:
-			onFloorArrival(newFloor)
-			
-		case newButtonPress := <-buttonPress:
+		case newButtonPress := <-event_orderButton:
 			onRequestButtonPress(newButtonPress)
+
+		case newFloor := <-event_floorArrival:
+			onFloorArrival(newFloor)
+
+		case obstruction := <-event_obstruction:
+			if obstruction {
+				fmt.Printf("Obstruction triggered!")
+			}
+
+		case emergencyStop := <-event_stopButton:
+			if emergencyStop {
+				fmt.Printf("Emergency stop button triggered!")
+			}
 		}
-
 	}
-
-
 }

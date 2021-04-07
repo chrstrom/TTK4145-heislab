3c3755b37c51948e5c901784dc037ab5d43c5755
Christopher Strøm <chstr@stud.ntnu.no>
2021-04-07 03:42:07 +0200

    Move for-select functionality to separate functions for clarity
    Signed-off-by: Christopher Strøm <chstr@stud.ntnu.no>

--- hallOrderManager/hallOrderManager.go
+++ hallOrderManager/hallOrderManager.go
@@ -9,6 +9,37 @@ import (
 	"../timer"
 )
 
+func OrderManager(
+	id string,
+	localRequestCh <-chan localOrderDelegation.LocalOrder,
+	channels network.NetworkChannels) {
+
+	manager := initializeManager(id, localRequestCh, channels)
+
+	for {
+		select {
+		case request := <-manager.localRequestChannel:
+			handleLocalRequest(request, manager)
+
+		case reply := <-manager.requestReplyFromNetwork:
+			handleReplyFromNetwork(reply, manager)
+
+		case confirm := <-manager.orderDelegationConfirmFromNetwork:
+			handleConfirmationFromNetwork(confirm, manager)
+
+		case order := <-manager.orderSyncFromNetwork:
+			synchronizeNetworkOrders(order, manager)
+
+		case orderID := <-manager.orderReplyTimeoutChannel:
+			delegateHallOrder(orderID, manager)
+
+		case orderID := <-manager.orderDelegationTimeoutChannel:
+			selfServeHallOrder(orderID, manager)
+		}
+	}
+}
+
+
 func initializeManager(
 	id string,
 	localRequestCh <-chan localOrderDelegation.LocalOrder,
@@ -37,107 +68,100 @@ func initializeManager(
 	return manager
 }
 
-func OrderManager(
-	id string,
-	localRequestCh <-chan localOrderDelegation.LocalOrder,
-	channels network.NetworkChannels) {
 
-	manager := initializeManager(id, localRequestCh, channels)
+func handleLocalRequest(request localOrderDelegation.LocalOrder, manager HallOrderManager) {
+	//Check if order already exits? Or is this better to do in localOrdermanager? Or allow duplicates
+	order := HallOrder{
+		ID:      manager.orderIDCounter,
+		OwnerID: manager.id,
+		State:   Received,
+		Floor:   request.Floor,
+		Dir:     request.Dir}
 
-	for {
-		select {
-		case request := <-manager.localRequestChannel:
-			//Check if order already exits? Or is this better to do in localOrdermanager? Or allow duplicates
-			order := HallOrder{
-				ID: manager.orderIDCounter,
-				OwnerID: manager.id,
-				State:   Received,
-				Floor: request.Floor,
-				Dir: request.Dir}
+	manager.orderIDCounter++
+	order.costs = make(map[string]int)
+	//get local elevator cost in some way
+	order.costs[manager.id] = rand.Intn(1000)
 
-			manager.orderIDCounter++
-			order.costs = make(map[string]int)
-			//get local elevator cost in some way
-			order.costs[manager.id] = rand.Intn(1000)
+	manager.orders.update(order)
+	fmt.Printf("%v - local request received \n", order.ID)
 
-			manager.orders.update(order)
-			fmt.Printf("%v - local request received \n", order.ID)
+	timer.SendWithDelay(orderReplyTime, manager.orderReplyTimeoutChannel, order.ID)
 
-			timer.SendWithDelay(orderReplyTime, manager.orderReplyTimeoutChannel, order.ID)
+	orderToNet := network.OrderStamped{
+		OrderID: order.ID,
+		Order:   network.Order{Floor: order.Floor, Dir: order.Dir}}
 
-			orderToNet := network.OrderStamped{
-				OrderID: order.ID,
+	manager.requestToNetwork <- orderToNet
+}
+
+func handleReplyFromNetwork(reply network.OrderStamped, manager HallOrderManager) {
+	order, valid := manager.orders.getOrder(manager.id, reply.OrderID)
+	if valid && order.State == Received {
+		order.costs[reply.ID] = reply.Order.Cost
+	}
+}
+func handleConfirmationFromNetwork(confirm network.OrderStamped, manager HallOrderManager) {
+	order, valid := manager.orders.getOrder(manager.id, confirm.OrderID)
+	if valid && order.State == Delegate {
+		order.State = Serving
+		fmt.Printf("%v - delegation confirmed \n", confirm.OrderID)
+
+		manager.orders.update(order)
+
+		//Let the elevators on the network know that this local elevator has taken an order
+		orderToNet := network.OrderSync{OrderID: order.ID, Floor: order.Floor, Dir: order.Dir}
+		manager.orderSyncToNetwork <- orderToNet
+	}	
+}
+
+func synchronizeNetworkOrders(order network.OrderSync, manager HallOrderManager) {
+	if order.ID != manager.id {
+		order := HallOrder{}
+		manager.orders.update(order)
+	}
+}
+
+func delegateHallOrder(orderID int, manager HallOrderManager) {
+	order, valid := manager.orders.getOrder(manager.id, orderID)
+	if valid && order.State == Received {
+		id := getIDOfLowestCost(order.costs)
+		if id == "" {
+			id = manager.id
+		}
+		order.DelegatedToID = id
+
+		if id == manager.id {
+			//send order to local elevator
+			fmt.Printf("%v - delegate to local elevator (%v replies) \n", orderID, len(order.costs))
+
+			order.State = Serving
+		} else {
+			fmt.Printf("%v - delegate to %v  (%v replies) \n", orderID, id, len(order.costs))
+			timer.SendWithDelay(orderDelegationTime, manager.orderDelegationTimeoutChannel, orderID)
+
+			order.State = Delegate
+
+			message := network.OrderStamped{
+				ID:      order.DelegatedToID,
+				OrderID: orderID,
 				Order:   network.Order{Floor: order.Floor, Dir: order.Dir}}
 
-			manager.requestToNetwork <- orderToNet
-
-		case reply := <-manager.requestReplyFromNetwork:
-			o, valid := manager.orders.getOrder(manager.id, reply.OrderID)
-			if valid && o.State == Received {
-				o.costs[reply.ID] = reply.Order.Cost
-			}
-
-		case confirm := <-manager.orderDelegationConfirmFromNetwork:
-			o, valid := manager.orders.getOrder(manager.id, confirm.OrderID)
-			if valid && o.State == Delegate {
-				o.State = Serving
-				fmt.Printf("%v - delegation confirmed \n", confirm.OrderID)
-
-				manager.orders.update(o)
-
-				//Let the elevators on the network know that this local elevator has taken an order
-				orderToNet := network.OrderSync{OrderID: o.ID, Floor: o.Floor, Dir: o.Dir}
-				manager.orderSyncToNetwork <- orderToNet
-			}
-
-		case sync := <-manager.orderSyncFromNetwork:
-			if sync.ID != manager.id {
-
-				order := HallOrder{}
-				manager.orders.update(order)
-			}
-
-		case orderID := <-manager.orderReplyTimeoutChannel:
-			o, valid := manager.orders.getOrder(manager.id, orderID)
-			if valid && o.State == Received {
-				id := getIDOfLowestCost(o.costs)
-				if id == "" {
-					id = manager.id
-				}
-				o.DelegatedToID = id
-
-				if id == manager.id {
-					//send order to local elevator
-					fmt.Printf("%v - delegate to local elevator (%v replies) \n", orderID, len(o.costs))
-
-					o.State = Serving
-				} else {
-					fmt.Printf("%v - delegate to %v  (%v replies) \n", orderID, id, len(o.costs))
-					timer.SendWithDelay(orderDelegationTime, manager.orderDelegationTimeoutChannel, orderID)
-
-					o.State = Delegate
-
-					message := network.OrderStamped{
-						ID:      o.DelegatedToID,
-						OrderID: orderID,
-						Order:   network.Order{Floor: o.Floor, Dir: o.Dir}}
-
-					manager.delegateToNetwork <- message
-				}
-				manager.orders.update(o)
-			}
-
-		case orderID := <-manager.orderDelegationTimeoutChannel:
-			o, valid := manager.orders.getOrder(manager.id, orderID)
-			if valid && o.State == Delegate {
-				//Send order to local elevator
-				o.DelegatedToID = manager.id
-				o.State = Serving
-
-				fmt.Printf("%v - delegation timedout! Sending to local elevator \n", orderID)
-
-				manager.orders.update(o)
-			}
+			manager.delegateToNetwork <- message
 		}
+		manager.orders.update(order)
+	}
+}
+
+func selfServeHallOrder(orderID int, manager HallOrderManager) {
+	order, valid := manager.orders.getOrder(manager.id, orderID)
+	if valid && order.State == Delegate {
+		//Send order to local elevator
+		order.DelegatedToID = manager.id
+		order.State = Serving
+
+		fmt.Printf("%v - delegation timedout! Sending to local elevator \n", orderID)
+
+		manager.orders.update(order)
 	}
 }

8db260c6e3a006df1dc5f8c45694c19aed963947
Christopher Strøm <chstr@stud.ntnu.no>
2021-04-15 14:52:03 +0200

    Name terminal windows
    Signed-off-by: Christopher Strøm <chstr@stud.ntnu.no>

--- simulator/ubuntu/run_2_nodes_with_simulators_ubuntu.sh
+++ simulator/ubuntu/run_2_nodes_with_simulators_ubuntu.sh
@@ -1,8 +1,8 @@
 
-gnome-terminal -- ./SimElevatorServer --port 15657
-gnome-terminal -- ./SimElevatorServer --port 15658
+gnome-terminal --title="ELEVATOR SERVER 1" -- ./SimElevatorServer --port 15657
+gnome-terminal --title="ELEVATOR SERVER 2" -- ./SimElevatorServer --port 15658
 
 cd ../..
 
-gnome-terminal -- go run main.go -port=15657
-gnome-terminal -- go run main.go -port=15658
+gnome-terminal --title="ELEVATOR NODE 1" -- go run main.go -port=15657
+gnome-terminal --title="ELEVATOR NODE 2" -- go run main.go -port=15658

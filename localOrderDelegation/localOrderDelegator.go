1d566ea3f5eb21542940098070bac4a46288bb22
Christopher Strøm <chstr@stud.ntnu.no>
2021-04-16 04:00:11 +0200

    Simplify local order delegator
    Signed-off-by: Christopher Strøm <chstr@stud.ntnu.no>

--- localOrderDelegation/localOrderDelegator.go
+++ localOrderDelegation/localOrderDelegator.go
@@ -1,42 +1,27 @@
 package localOrderDelegation
 
-import (
-	"fmt"
+import "../elevio"
 
-	"../elevio"
-)
-
-func initializeDelegator(
-	buttonCh <-chan elevio.ButtonEvent,
-	cabOrderCh chan<- int,
-	hallOrderCh chan<- LocalOrder) Delegator {
-
-	var delegator Delegator
-
-	delegator.buttonInputChannel = buttonCh
-	delegator.cabOrderChannel = cabOrderCh
-	delegator.hallOrderChannel = hallOrderCh
-
-	return delegator
-}
-
+// This is the driver function for the local delegation node
+// and contains a for-select, thus should be called as a goroutine.
 func OrderDelegator(
 	buttonCh <-chan elevio.ButtonEvent,
 	cabOrderCh chan<- int,
 	hallOrderCh chan<- LocalOrder) {
 
-	delegator := initializeDelegator(buttonCh, cabOrderCh, hallOrderCh)
+	delegator := Delegator{
+		buttonInputChannel: buttonCh,
+		cabOrderChannel:    cabOrderCh,
+		hallOrderChannel:   hallOrderCh}
 
 	for {
 		select {
 		case buttonEvent := <-delegator.buttonInputChannel:
 			if buttonEvent.Button == elevio.BT_Cab {
 				delegator.cabOrderChannel <- buttonEvent.Floor
-				fmt.Printf("Send cab order to floor %v\n", buttonEvent.Floor)
 			} else {
 				order := LocalOrder{Floor: buttonEvent.Floor, Dir: int(buttonEvent.Button)}
 				delegator.hallOrderChannel <- order
-				fmt.Printf("Send order to floor %v with direction %v\n", buttonEvent.Floor, buttonEvent.Button)
 			}
 		}
 	}

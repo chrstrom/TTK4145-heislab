ff2fb252bb01d618c562d711898ba8a569c81e70
Christopher Strøm <chstr@stud.ntnu.no>
2021-04-16 03:30:33 +0200

    Remove redundant functions in hall order manager, and clean up naming
    Signed-off-by: Christopher Strøm <chstr@stud.ntnu.no>

--- hallOrderManager/hallOrderManager.go
+++ hallOrderManager/hallOrderManager.go
@@ -1,20 +1,16 @@
 package hallOrderManager
 
 import (
-	"fmt"
-	"log"
-
-	"os"
-
+	"../config"
 	"../elevio"
 	"../localOrderDelegation"
-	"../network/peers"
 	msg "../orderTypes"
 	"../timer"
 	"../utility"
-	"../config"
 )
 
+// This is the driver function for the hall order manager
+// and contains a for-select, thus should be called as a goroutine.
 func OrderManager(
 	id string,
 	localRequestCh <-chan localOrderDelegation.LocalOrder,
@@ -26,277 +22,238 @@ func OrderManager(
 	for {
 		select {
 		case request := <-manager.localRequestChannel:
-			handleLocalRequest(request, &manager)
 
-		case buttonEvent := <-manager.orderComplete:
-			completeOrder(buttonEvent, &manager)
+			for _, order := range manager.orders.getOrdersToFloorWithDir(request.Floor, request.Dir) {
+				if order.State != msg.Completed {
+					return
+				}
+			}
 
-		case reply := <-manager.replyToRequestFromNetwork:
-			handleReplyFromNetwork(reply, &manager)
+			order := msg.HallOrder{
+				OwnerID: manager.id,
+				ID:      manager.orderIDCounter,
+				State:   msg.Received,
+				Floor:   request.Floor,
+				Dir:     request.Dir}
 
-		case confirm := <-manager.orderDelegationConfirmFromNetwork:
-			handleConfirmationFromNetwork(confirm, &manager)
+			manager.orderIDCounter++
+			order.Costs = make(map[string]int)
 
-		case delegation := <-manager.delegationFromNetwork:
-			acceptDelegatedHallOrder(delegation, &manager)
+			manager.requestElevatorCost <- msg.RequestCost{
+				Order: msg.OrderStamped{
+					OrderID: order.ID,
+					Order:   msg.Order{Floor: order.Floor, Dir: order.Dir}},
+				RequestFrom: msg.HallOrderManager}
 
-		case sync := <-manager.orderSyncFromNetwork:
-			synchronizeOrderFromNetwork(sync, &manager)
+			order.Costs[manager.id] = <-manager.elevatorCost
+			manager.orders.update(order)
 
-		case orderID := <-manager.orderReplyTimeoutChannel:
-			delegateHallOrder(orderID, &manager)
-
-		case orderID := <-manager.orderDelegationTimeoutChannel:
-			selfServeHallOrder(orderID, &manager)
-
-		case order := <-manager.orderCompleteTimeoutChannel:
-			handleOrderCompleteTimeout(order, &manager)
-
-		case peerUpdate := <-manager.peerUpdateChannel:
-			handlePeerUpdate(peerUpdate, &manager)
-
-			//case <-time.After(time.Second * 5):
-			//	manager.logger.Printf("Quiet for 5 seconds")
-		}
-	}
-}
-
-func initializeManager(
-	id string,
-	localRequestCh <-chan localOrderDelegation.LocalOrder,
-	fsmChannels msg.FSMChannels,
-	channels msg.NetworkChannels) HallOrderManager {
-
-	var manager HallOrderManager
-
-	manager.id = id
-
-	manager.orders = make(OrderMap)
-	manager.orderIDCounter = 1
-
-	manager.localRequestChannel = localRequestCh
-
-	manager.requestToNetwork = channels.RequestToNetwork
-	manager.delegateToNetwork = channels.DelegateOrderToNetwork
-	manager.orderSyncToNetwork = channels.SyncOrderToNetwork
-	manager.delegationConfirmToNetwork = channels.DelegationConfirmToNetwork
-
-	manager.replyToRequestFromNetwork = channels.ReplyToRequestFromNetwork
-	manager.orderDelegationConfirmFromNetwork = channels.DelegationConfirmFromNetwork
-	manager.orderSyncFromNetwork = channels.SyncOrderFromNetwork
-	manager.orderDelegationConfirmFromNetwork = channels.DelegationConfirmFromNetwork
-	manager.delegationFromNetwork = channels.DelegateFromNetwork
-	manager.peerUpdateChannel = channels.PeerUpdate
-
-	manager.delegateToLocalElevator = fsmChannels.DelegateHallOrder
-	manager.elevatorCost = fsmChannels.ReplyToHallOrderManager
-	manager.requestElevatorCost = fsmChannels.RequestCost
-	manager.orderComplete = fsmChannels.OrderComplete
-
-	manager.orderReplyTimeoutChannel = make(chan int)
-	manager.orderDelegationTimeoutChannel = make(chan int)
-	manager.orderCompleteTimeoutChannel = make(chan msg.HallOrder)
-
-	filepath := "log/" + manager.id + "-hallOrderManager.log"
-	file, _ := os.Create(filepath)
-	manager.logger = log.New(file, "", log.Ltime|log.Lmicroseconds)
-
-	// Turn off all hall lights on init
-	for f := 0; f < config.N_FLOORS; f++ {
-		for b := elevio.ButtonType(0); b < 2; b++ {
-			elevio.SetButtonLamp(b, f, false)
-		}
-	}
-
-	return manager
-}
-
-func handleLocalRequest(request localOrderDelegation.LocalOrder, manager *HallOrderManager) {
-	for _, o := range manager.orders.getOrdersToFloorWithDir(request.Floor, request.Dir) {
-		if o.State != msg.Completed {
-			return
-		}
-	}
-
-	order := msg.HallOrder{
-		OwnerID: manager.id,
-		ID:      manager.orderIDCounter,
-		State:   msg.Received,
-		Floor:   request.Floor,
-		Dir:     request.Dir}
-
-	manager.orderIDCounter++
-	order.Costs = make(map[string]int)
-
-	orderToFSM := msg.OrderStamped{
-		OrderID: order.ID,
-		Order:   msg.Order{Floor: order.Floor, Dir: order.Dir}}
-
-	manager.requestElevatorCost <- msg.RequestCost{Order: orderToFSM, RequestFrom: msg.HallOrderManager}
-
-	order.Costs[manager.id] = <-manager.elevatorCost
-	fmt.Printf("Cost:%v\n", order.Costs[manager.id])
-
-	manager.orders.update(order)
-
-	timer.SendWithDelay(config.ORDER_REPLY_TIME, manager.orderReplyTimeoutChannel, order.ID)
-	timer.SendWithDelayHallOrder(config.ORDER_COMPLETION_TIMEOUT, manager.orderCompleteTimeoutChannel, order)
-
-	orderToNet := msg.OrderStamped{
-		OrderID: order.ID,
-		Order:   msg.Order{Floor: order.Floor, Dir: order.Dir}}
-
-	manager.logger.Printf("New order ID%v: %#v", order.ID, order)
-	manager.requestToNetwork <- orderToNet
-}
-
-func completeOrder(buttonEvent elevio.ButtonEvent, manager *HallOrderManager) {
-
-	dir := int(buttonEvent.Button)
-	floor := buttonEvent.Floor
-
-	setHallLight(dir, floor, false)
-	for _, order := range manager.orders.getOrdersToFloorWithDir(floor, dir) {
-		order.State = msg.Completed
-		manager.orders.update(order)
-		manager.logger.Printf("Order completed ID%v: %#v\n", order.ID, order)
-		orderStateBroadcast(order, manager)
-	}
-
-}
-
-func handleReplyFromNetwork(reply msg.OrderStamped, manager *HallOrderManager) {
-	order, valid := manager.orders.getOrder(manager.id, reply.OrderID)
-	if valid && order.State == msg.Received {
-		order.Costs[reply.ID] = reply.Order.Cost
-		manager.orders.update(order)
-		manager.logger.Printf("New reply to order ID%v: %#v", order.ID, order)
-	}
-}
-
-func handleConfirmationFromNetwork(confirm msg.OrderStamped, manager *HallOrderManager) {
-	order, valid := manager.orders.getOrder(manager.id, confirm.OrderID)
-	if valid && order.State == msg.Delegate {
-		order.State = msg.Serving
-		//fmt.Printf("%v - delegation confirmed \n", confirm.OrderID)
-
-		manager.orders.update(order)
-
-		manager.logger.Printf("Confirmed ID%v: %#v", order.ID, order)
-		//Let the elevators on the network know that this local elevator has taken an order
-		orderStateBroadcast(order, manager)
-
-		setHallLight(order.Dir, order.Floor, true)
-	}
-}
-
-func acceptDelegatedHallOrder(delegation msg.OrderStamped, manager *HallOrderManager) {
-	order := msg.HallOrder{OwnerID: delegation.ID,
-		ID:            delegation.OrderID,
-		DelegatedToID: manager.id,
-		State:         msg.Serving,
-		Floor:         delegation.Order.Floor,
-		Dir:           delegation.Order.Dir}
-
-	manager.orders.update(order)
-	manager.logger.Printf("Received order from net: %#v", order)
-	manager.delegateToLocalElevator <- elevio.ButtonEvent{Floor: delegation.Order.Floor, Button: elevio.ButtonType(delegation.Order.Dir)}
-
-	reply := msg.OrderStamped{
-		ID:      order.OwnerID,
-		OrderID: order.ID,
-		Order:   msg.Order{Floor: order.Floor, Dir: order.Dir}}
-	manager.delegationConfirmToNetwork <- reply
-
-	setHallLight(order.Dir, order.Floor, true)
-}
-
-func synchronizeOrderFromNetwork(order msg.HallOrder, manager *HallOrderManager) {
-	orderSaved, exists := manager.orders.getOrder(order.OwnerID, order.ID)
-
-	if !exists || (exists && order.State >= orderSaved.State) {
-
-		if !exists {
+			timer.SendWithDelay(config.ORDER_REPLY_TIME, manager.orderReplyTimeoutChannel, order.ID)
 			timer.SendWithDelayHallOrder(config.ORDER_COMPLETION_TIMEOUT, manager.orderCompleteTimeoutChannel, order)
-		}
-		manager.logger.Printf("Sync from net: %#v", order)
 
-		manager.orders.update(order)
-
-		if order.State == msg.Serving {
-			setHallLight(order.Dir, order.Floor, true)
-		}
-
-		if order.State == msg.Completed {
-			setHallLight(order.Dir, order.Floor, false)
-		}
-
-	}
-}
-
-func delegateHallOrder(orderID int, manager *HallOrderManager) {
-	order, valid := manager.orders.getOrder(manager.id, orderID)
-	if valid && order.State == msg.Received {
-		id := getIDOfLowestCost(order.Costs)
-		if id == "" {
-			id = manager.id
-		}
-		order.DelegatedToID = id
-
-		if id == manager.id {
-			//send order to local elevator
-			manager.delegateToLocalElevator <- elevio.ButtonEvent{Floor: order.Floor, Button: elevio.ButtonType(order.Dir)}
-
-			manager.logger.Printf("Delegate order ID%v to local elevator (%v replies): %#v", order.ID, len(order.Costs), order)
-			order.State = msg.Serving
-			orderStateBroadcast(order, manager)
-
-			setHallLight(order.Dir, order.Floor, true)
-		} else {
-			manager.logger.Printf("Delegate order ID%v to net (%v replies): %#v", order.ID, len(order.Costs), order)
-			timer.SendWithDelay(config.ORDER_DELEGATION_TIME, manager.orderDelegationTimeoutChannel, orderID)
-
-			order.State = msg.Delegate
-
-			message := msg.OrderStamped{
-				ID:      order.DelegatedToID,
-				OrderID: orderID,
+			orderToNet := msg.OrderStamped{
+				OrderID: order.ID,
 				Order:   msg.Order{Floor: order.Floor, Dir: order.Dir}}
 
-			manager.delegateToNetwork <- message
+			manager.logger.Printf("New order ID%v: %#v", order.ID, order)
+			manager.requestToNetwork <- orderToNet
+
+		case buttonEvent := <-manager.orderComplete:
+
+			dir := int(buttonEvent.Button)
+			floor := buttonEvent.Floor
+
+			setHallLight(dir, floor, false)
+
+			for _, order := range manager.orders.getOrdersToFloorWithDir(floor, dir) {
+				order.State = msg.Completed
+				manager.orders.update(order)
+				manager.logger.Printf("Order completed ID%v: %#v\n", order.ID, order)
+				syncOrderWithOtherElevators(order, &manager)
+			}
+
+		case reply := <-manager.replyToRequestFromNetwork:
+
+			order, orderIsValid := manager.orders.getOrder(manager.id, reply.OrderID)
+
+			if orderIsValid && order.State == msg.Received {
+				order.Costs[reply.ID] = reply.Order.Cost
+				manager.orders.update(order)
+				manager.logger.Printf("New reply to order ID%v: %#v", order.ID, order)
+			}
+
+		case confirm := <-manager.orderDelegationConfirmFromNetwork:
+
+			order, orderIsValid := manager.orders.getOrder(manager.id, confirm.OrderID)
+
+			if orderIsValid && order.State == msg.Delegate {
+				order.State = msg.Serving
+				manager.orders.update(order)
+				manager.logger.Printf("Confirmed ID%v: %#v", order.ID, order)
+				syncOrderWithOtherElevators(order, &manager)
+
+				setHallLight(order.Dir, order.Floor, true)
+			}
+
+		case delegation := <-manager.delegationFromNetwork:
+
+			incomingOrder := msg.HallOrder{
+				OwnerID:       delegation.ID,
+				ID:            delegation.OrderID,
+				DelegatedToID: manager.id,
+				State:         msg.Serving,
+				Floor:         delegation.Order.Floor,
+				Dir:           delegation.Order.Dir}
+
+			manager.orders.update(incomingOrder)
+			manager.logger.Printf("Received order from net: %#v", incomingOrder)
+
+			orderForFSM := elevio.ButtonEvent{
+				Floor:  delegation.Order.Floor,
+				Button: elevio.ButtonType(delegation.Order.Dir)}
+
+			manager.delegateToLocalElevator <- orderForFSM
+
+			replyToNetwork := msg.OrderStamped{
+				ID:      incomingOrder.OwnerID,
+				OrderID: incomingOrder.ID,
+				Order:   msg.Order{Floor: incomingOrder.Floor, Dir: incomingOrder.Dir}}
+			manager.delegationConfirmToNetwork <- replyToNetwork
+
+			setHallLight(incomingOrder.Dir, incomingOrder.Floor, true)
+
+		case order := <-manager.orderSyncFromNetwork:
+
+			orderSaved, orderExists := manager.orders.getOrder(order.OwnerID, order.ID)
+
+			// Conditionally synchronize the order itself
+			if !orderExists || (orderExists && order.State >= orderSaved.State) {
+
+				if !orderExists {
+					timer.SendWithDelayHallOrder(
+						config.ORDER_COMPLETION_TIMEOUT,
+						manager.orderCompleteTimeoutChannel,
+						order)
+				}
+				manager.orders.update(order)
+				manager.logger.Printf("Sync from net: %#v", order)
+
+				// Make sure lights are updated in accordance with the synched order
+				if order.State == msg.Serving {
+					setHallLight(order.Dir, order.Floor, true)
+				}
+
+				if order.State == msg.Completed {
+					setHallLight(order.Dir, order.Floor, false)
+				}
+
+			}
+
+		case orderID := <-manager.orderReplyTimeoutChannel:
+			// Once the window of time we listen to replies runs out,
+			// we can start delegating the hall order
+
+			order, orderIsValid := manager.orders.getOrder(manager.id, orderID)
+
+			if orderIsValid && order.State == msg.Received {
+				id := getIDOfLowestCost(order.Costs, manager.id)
+
+				order.DelegatedToID = id
+
+				if id == manager.id {
+					selfServeHallOrder(order, &manager)
+					order.State = msg.Serving
+
+					manager.logger.Printf("Timeout delegation ID%v, sending to local elevator: %v", order.ID, order)
+
+					syncOrderWithOtherElevators(order, &manager)
+					setHallLight(order.Dir, order.Floor, true)
+
+				} else { // Order will be given to another elevator on the network
+					manager.logger.Printf("Delegate order ID%v to net (%v replies): %#v", order.ID, len(order.Costs), order)
+					timer.SendWithDelay(config.ORDER_DELEGATION_TIME, manager.orderDelegationTimeoutChannel, orderID)
+
+					order.State = msg.Delegate
+
+					delegatedOrder := msg.OrderStamped{
+						ID:      order.DelegatedToID,
+						OrderID: orderID,
+						Order:   msg.Order{Floor: order.Floor, Dir: order.Dir}}
+
+					manager.delegateToNetwork <- delegatedOrder
+				}
+				manager.orders.update(order)
+			}
+
+		case orderID := <-manager.orderDelegationTimeoutChannel:
+
+			order, orderIsValid := manager.orders.getOrder(manager.id, orderID)
+
+			if orderIsValid && order.State == msg.Delegate {
+
+				order.DelegatedToID = manager.id
+				selfServeHallOrder(order, &manager)
+				order.State = msg.Serving
+
+				manager.logger.Printf("Timeout delegation ID%v, sending to local elevator: %v", order.ID, order)
+
+				manager.orders.update(order)
+				syncOrderWithOtherElevators(order, &manager)
+				setHallLight(order.Dir, order.Floor, true)
+			}
+
+		case order := <-manager.orderCompleteTimeoutChannel:
+
+			orderSaved, orderExists := manager.orders.getOrder(order.OwnerID, order.ID)
+
+			if orderExists && orderSaved.State != msg.Completed {
+				manager.logger.Printf("Order timeout ID%v: %#v", order.ID, order)
+				selfServeHallOrder(order, &manager)
+			}
+
+		case peerUpdate := <-manager.peerUpdateChannel:
+			for _, nodeid := range peerUpdate.Lost {
+
+				manager.logger.Printf("Node lost connection: %v", nodeid)
+				orders := manager.orders.getOrderToID(nodeid)
+
+				for _, order := range orders {
+
+					if order.OwnerID == manager.id {
+						redelegateOrder(order, &manager)
+
+					} else if !utility.IsStringInSlice(order.OwnerID, peerUpdate.Peers) &&
+						!utility.IsStringInSlice(order.DelegatedToID, peerUpdate.Peers) {
+
+						redelegateOrder(order, &manager)
+					}
+				}
+			}
+
+			if len(peerUpdate.New) > 0 {
+				manager.logger.Printf("New node(s) connected")
+				orders := manager.orders.getOrderFromID(manager.id)
+				for _, order := range orders {
+					if order.State == msg.Serving {
+						syncOrderWithOtherElevators(order, &manager)
+					}
+				}
+			}
 		}
-		manager.orders.update(order)
 	}
 }
 
-func selfServeHallOrder(orderID int, manager *HallOrderManager) {
-	order, valid := manager.orders.getOrder(manager.id, orderID)
-	if valid && order.State == msg.Delegate {
-		//Send order to local elevator
-		manager.delegateToLocalElevator <- elevio.ButtonEvent{Floor: order.Floor, Button: elevio.ButtonType(order.Dir)}
-		order.DelegatedToID = manager.id
-		order.State = msg.Serving
+func selfServeHallOrder(order msg.HallOrder, manager *HallOrderManager) {
+	orderToFSM := elevio.ButtonEvent{
+		Floor:  order.Floor,
+		Button: elevio.ButtonType(order.Dir)}
 
-		manager.logger.Printf("Timeout delegation ID%v, sending to local elevator: %v", order.ID, order)
-
-		manager.orders.update(order)
-		orderStateBroadcast(order, manager)
-		setHallLight(order.Dir, order.Floor, true)
-	}
+	manager.delegateToLocalElevator <- orderToFSM
 }
 
-func orderStateBroadcast(order msg.HallOrder, manager *HallOrderManager) {
+func syncOrderWithOtherElevators(order msg.HallOrder, manager *HallOrderManager) {
 	manager.orderSyncToNetwork <- order
 	manager.logger.Printf("Sync order ID%v to net:%#v", order.ID, order)
-}
-
-func handleOrderCompleteTimeout(order msg.HallOrder, manager *HallOrderManager) {
-	orderSaved, exists := manager.orders.getOrder(order.OwnerID, order.ID)
-	if exists && orderSaved.State != msg.Completed {
-		manager.logger.Printf("Order timeout ID%v: %#v", order.ID, order)
-		manager.delegateToLocalElevator <- elevio.ButtonEvent{Floor: order.Floor, Button: elevio.ButtonType(order.Dir)}
-		//redelegateOrder(order, manager)
-	}
 }
 
 func redelegateOrder(o msg.HallOrder, manager *HallOrderManager) {
@@ -312,11 +269,11 @@ func redelegateOrder(o msg.HallOrder, manager *HallOrderManager) {
 		order.DelegatedToID = ""
 		order.State = msg.Received
 
-		orderToFSM := msg.OrderStamped{
-			OrderID: order.ID,
-			Order:   msg.Order{Floor: order.Floor, Dir: order.Dir}}
-
-		manager.requestElevatorCost <- msg.RequestCost{Order: orderToFSM, RequestFrom: msg.HallOrderManager}
+		manager.requestElevatorCost <- msg.RequestCost{
+			Order: msg.OrderStamped{
+				OrderID: order.ID,
+				Order:   msg.Order{Floor: order.Floor, Dir: order.Dir}},
+			RequestFrom: msg.HallOrderManager}
 
 		order.Costs[manager.id] = <-manager.elevatorCost
 
@@ -330,29 +287,5 @@ func redelegateOrder(o msg.HallOrder, manager *HallOrderManager) {
 			Order:   msg.Order{Floor: order.Floor, Dir: order.Dir}}
 
 		manager.requestToNetwork <- orderToNet
-	}
-}
-
-func handlePeerUpdate(peerUpdate peers.PeerUpdate, manager *HallOrderManager) {
-	for _, nodeid := range peerUpdate.Lost {
-		manager.logger.Printf("Node lost connection: %v", nodeid)
-		orders := manager.orders.getOrderToID(nodeid)
-		for _, o := range orders {
-			if o.OwnerID == manager.id {
-				redelegateOrder(o, manager)
-			} else if !utility.IsStringInSlice(o.OwnerID, peerUpdate.Peers) && !utility.IsStringInSlice(o.DelegatedToID, peerUpdate.Peers) {
-				redelegateOrder(o, manager)
-			}
-		}
-	}
-
-	if len(peerUpdate.New) > 0 {
-		manager.logger.Printf("New node(s) connected")
-		orders := manager.orders.getOrderFromID(manager.id)
-		for _, o := range orders {
-			if o.State == msg.Serving {
-				orderStateBroadcast(o, manager)
-			}
-		}
 	}
 }
--- hallOrderManager/hallOrderUtility.go
+++ hallOrderManager/hallOrderUtility.go
@@ -1,10 +1,16 @@
 package hallOrderManager
 
 import (
-	io "../elevio"
+	"log"
+	"os"
+
+	"../config"
+	"../elevio"
+	"../localOrderDelegation"
+	msg "../orderTypes"
 )
 
-func getIDOfLowestCost(costs map[string]int) string {
+func getIDOfLowestCost(costs map[string]int, defaultID string) string {
 	lowest := 100000000
 	lowestID := ""
 
@@ -14,9 +20,64 @@ func getIDOfLowestCost(costs map[string]int) string {
 			lowestID = id
 		}
 	}
+
+	if lowestID == "" {
+		lowestID = defaultID
+	}
+	
 	return lowestID
 }
 
 func setHallLight(dir int, floor int, state bool) {
-	io.SetButtonLamp(io.ButtonType(dir), floor, state)
+	elevio.SetButtonLamp(elevio.ButtonType(dir), floor, state)
+}
+
+func initializeManager(
+	id string,
+	localRequestCh <-chan localOrderDelegation.LocalOrder,
+	fsmChannels msg.FSMChannels,
+	channels msg.NetworkChannels) HallOrderManager {
+
+	var manager HallOrderManager
+
+	manager.id = id
+
+	manager.orders = make(OrderMap)
+	manager.orderIDCounter = 1
+
+	manager.localRequestChannel = localRequestCh
+
+	manager.requestToNetwork = channels.RequestToNetwork
+	manager.delegateToNetwork = channels.DelegateOrderToNetwork
+	manager.orderSyncToNetwork = channels.SyncOrderToNetwork
+	manager.delegationConfirmToNetwork = channels.DelegationConfirmToNetwork
+
+	manager.replyToRequestFromNetwork = channels.ReplyToRequestFromNetwork
+	manager.orderDelegationConfirmFromNetwork = channels.DelegationConfirmFromNetwork
+	manager.orderSyncFromNetwork = channels.SyncOrderFromNetwork
+	manager.orderDelegationConfirmFromNetwork = channels.DelegationConfirmFromNetwork
+	manager.delegationFromNetwork = channels.DelegateFromNetwork
+	manager.peerUpdateChannel = channels.PeerUpdate
+
+	manager.delegateToLocalElevator = fsmChannels.DelegateHallOrder
+	manager.elevatorCost = fsmChannels.ReplyToHallOrderManager
+	manager.requestElevatorCost = fsmChannels.RequestCost
+	manager.orderComplete = fsmChannels.OrderComplete
+
+	manager.orderReplyTimeoutChannel = make(chan int)
+	manager.orderDelegationTimeoutChannel = make(chan int)
+	manager.orderCompleteTimeoutChannel = make(chan msg.HallOrder)
+
+	filepath := "log/" + manager.id + "-hallOrderManager.log"
+	file, _ := os.Create(filepath)
+	manager.logger = log.New(file, "", log.Ltime|log.Lmicroseconds)
+
+	// Turn off all hall lights on init
+	for f := 0; f < config.N_FLOORS; f++ {
+		for b := elevio.ButtonType(0); b < 2; b++ {
+			elevio.SetButtonLamp(b, f, false)
+		}
+	}
+
+	return manager
 }

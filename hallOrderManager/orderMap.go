86045ba73f946a54e383ab6cbb10373d940f71e9
Christopher Strøm <chstr@stud.ntnu.no>
2021-04-16 04:24:16 +0200

    Comment cleanup
    Signed-off-by: Christopher Strøm <chstr@stud.ntnu.no>

--- hallOrderManager/orderMap.go
+++ hallOrderManager/orderMap.go
@@ -30,19 +30,6 @@ func (om OrderMap) getOrder(ownerID string, orderID int) (order msg.HallOrder, f
 	return msg.HallOrder{}, false
 }
 
-/*func (om OrderMap) getOrderFromIDtoID(ownerID, delegatedID string) []msg.HallOrder {
-	var orders []msg.HallOrder
-	_, ok := om[ownerID]
-	if ok {
-		for _, o := range om[ownerID] {
-			if o.DelegatedToID == delegatedID {
-				orders = append(orders, o)
-			}
-		}
-	}
-	return orders
-}*/
-
 func (om OrderMap) getOrderFromID(ownerID string) []msg.HallOrder {
 	var orders []msg.HallOrder
 	_, ok := om[ownerID]
@@ -128,28 +115,3 @@ func (om OrderMap) printOrderMap() {
 		fmt.Printf("\n\n")
 	}
 }
-
-/*
-func (om OrderMap) isValidOrder(order Order) bool {
-	_, ok := om[order.OwnerID]
-	if ok {
-		o, ok2 := om[order.OwnerID][order.ID]
-		if ok2 && o.ID == order.ID && o.Floor == order.Floor && o.Dir == order.Dir {
-			return true
-		}
-	}
-	return false
-}*/
-/*
-func TestOrderMap() {
-	om := make(OrderMap)
-	o := Order{ID: 123, State: Received, Floor: 2, Dir: 1, OwnerID: "node1"}
-	//o2 := Order{ID: 123, State: Received, Floor: 2, Dir: 1, OwnerID: "node1"}
-	om.update(o)
-	fmt.Println(om)
-	fmt.Println(om.getOrder("node1", 123))
-	o3, _ := om.getOrder("node1", 123)
-	o3.Floor = 20
-	om.update(o3)
-	fmt.Println(om)
-}*/
